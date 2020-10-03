dba408426b191ba6dffb323dd3c560e9114a9d85
brianlam37 <brianlam37@gmail.com>
2020-10-03 02:48:54 -0700

    comments schema and route

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,16 +2,37 @@ const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
+const Comment = require('../models/comment');
 
 
 blogsRouter.get('/', async (request, response) => {
-	const blogs = await Blog.find({}).populate('user', {username: 1, name: 1});
+	const blogs = await Blog.find({})
+		.populate('user', {username: 1, name: 1})
+		.populate('comments');
 	response.json(blogs);
 });
 
 blogsRouter.get('/:id', async (request, response) => {
 	const blogs = await Blog.findById(request.params.id);
 	response.json(blogs);
+});
+
+blogsRouter.post('/:id/comments', async (request, response) => {
+
+	const body = request.body;
+	const decodedToken = jwt.verify(request.token, process.env.SECRET);
+	if (!request.token || !decodedToken.id) {
+		return response.status(401).json({error: 'token missing or invalid'});
+	}
+	const blog = await Blog.findById(request.params.id);
+	const comment = new Comment({
+		content: body.content,
+		blog: blog._id
+	});
+	const postedComment = await comment.save();
+	blog.comments = blog.comments.concat(postedComment._id);
+	await blog.save();
+	response.status(201).json(postedComment);
 });
 
 blogsRouter.post('/', async (request, response) => {
